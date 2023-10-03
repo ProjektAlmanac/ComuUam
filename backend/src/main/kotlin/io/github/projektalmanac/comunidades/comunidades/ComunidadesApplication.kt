b8efc9fbbd837d6a8d30bc5374cd231b1d5bad76
Antar <antar.espadas@hotmail.com>
2023-10-03 11:52:48 -0600

    Initial commit

---
+++ backend/src/main/kotlin/io/github/projektalmanac/comunidades/comunidades/ComunidadesApplication.kt
@@ -0,0 +1,11 @@
+package io.github.projektalmanac.comunidades.comunidades
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ComunidadesApplication
+
+fun main(args: Array<String>) {
+	runApplication<ComunidadesApplication>(*args)
+}
---
+++ backend/src/test/kotlin/io/github/projektalmanac/comunidades/comunidades/ComunidadesApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.github.projektalmanac.comunidades.comunidades
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ComunidadesApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
