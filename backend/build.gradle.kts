04d2ce8e1f63568b3a40121db464d672551dbc94
Antar <antar.espadas@hotmail.com>
2023-10-03 15:42:03 -0600

    Añade openapi generator a backend

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -6,6 +6,8 @@ plugins {
 	kotlin("jvm") version "1.8.22"
 	kotlin("plugin.spring") version "1.8.22"
 	kotlin("plugin.jpa") version "1.8.22"
+
+	id("org.openapi.generator") version "7.0.1"
 }
 
 group = "io.github.projektalmanac.comunidades"
@@ -20,15 +22,39 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-starter-security")
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	runtimeOnly("com.h2database:h2")
-	runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.springframework.security:spring-security-test")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    runtimeOnly("com.h2database:h2")
+    runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.security:spring-security-test")
+
+
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springdoc:springdoc-openapi-ui:1.6.8")
+
+    implementation("com.google.code.findbugs:jsr305:3.0.2")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-xml")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("jakarta.validation:jakarta.validation-api")
+    implementation("jakarta.annotation:jakarta.annotation-api:2.1.0")
+
+    implementation("javax.validation:validation-api:2.0.1.Final")
+
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testImplementation("org.springframework.boot:spring-boot-starter-test") {
+        exclude(module = "junit")
+
+    }
 }
 
 tasks.withType<KotlinCompile> {
@@ -40,4 +66,22 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+
+openApiGenerate {
+	generatorName = "kotlin-spring"
+	inputSpec = "$rootDir/../api-docs.yml"
+	outputDir = "$rootDir"
+	groupId = "$group"
+	modelNameSuffix = "Dto"
+
+	packageName = "io.github.projektalmanac.comunidades.generated"
+	apiPackage = "io.github.projektalmanac.comunidades.generated.api"
+	modelPackage = "io.github.projektalmanac.comunidades.generated.dto"
+
+	additionalProperties.put("interfaceOnly", true)
+	additionalProperties.put("skipDefaultInterface", true)
+	additionalProperties.put("configPackage", "io.github.projektalmanac.comunidades.generated.config")
+	additionalProperties.put("hideGenerationTimestamp", true)
 }
