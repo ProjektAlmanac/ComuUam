1cc9b9a4ac51141146ad8830ef6617838a354820
Antar Espadas <antar.espadas@hotmail.com>
2023-10-04 03:39:41 -0600

    Implementa HU-03

--- backend/src/main/kotlin/io/github/projektalmanac/comunidades/exception/Exceptions.kt
+++ backend/src/main/kotlin/io/github/projektalmanac/comunidades/exception/Exceptions.kt
@@ -1,3 +1,5 @@
 package io.github.projektalmanac.comunidades.exception
 
 class UserNotFoundException(userId: Int) : RuntimeException("No se encontró ningún usuario con id $userId.")
+
+class UserAlreadyExistsException(email: String) : RuntimeException("Ya existe un usuario con el correo $email.")
--- backend/src/main/kotlin/io/github/projektalmanac/comunidades/mapper/UserMapper.kt
+++ backend/src/main/kotlin/io/github/projektalmanac/comunidades/mapper/UserMapper.kt
@@ -1,6 +1,10 @@
 package io.github.projektalmanac.comunidades.mapper
 
+import io.github.projektalmanac.comunidades.entity.User
+import io.github.projektalmanac.comunidades.generated.dto.CreacionUsuarioDto
+import io.github.projektalmanac.comunidades.generated.dto.UsuarioCreadoDto
 import org.mapstruct.Mapper
+import org.mapstruct.Mapping
 import org.mapstruct.factory.Mappers
 
 @Mapper
@@ -8,4 +12,13 @@ interface UserMapper {
     companion object {
         val INSTANCE = Mappers.getMapper(UserMapper::class.java)
     }
+
+    @Mapping(source = "nombre", target = "name")
+    @Mapping(source = "apellidos", target = "lastName")
+    fun toEntity(dto: CreacionUsuarioDto): User
+
+    @Mapping(source = "name", target = "nombre")
+    @Mapping(source = "lastName", target = "apellidos")
+    @Mapping(source = "idUser", target = "id")
+    fun toDto(entity: User): UsuarioCreadoDto
 }
