674b4657d5bb47a5e9faf9db1ae1aabe5b1e3e47
Antar Espadas <antar.espadas@hotmail.com>
2023-10-04 10:46:06 -0600

    Añade vista para HU-05

--- frontend/src/routes/crear-comunidad/+page.svelte
+++ frontend/src/routes/crear-comunidad/+page.svelte
@@ -1,31 +1,74 @@
 <script>
-    let nombre = "";
-    let visibilidad = "publico";
-    let descripcion = "";
+	import Textfield from '@smui/textfield';
+	import FormField from '@smui/form-field';
+	import Radio from '@smui/radio';
+	import Button, { Label } from '@smui/button';
+	import LayoutGrid, { Cell } from '@smui/layout-grid';
 
-    function crearComunidad() {
+	let nombre = '';
+	let visibilidad = 'Publico';
+	let descripcion = '';
 
-        console.log("Nombre:", nombre);
-        console.log("Visibilidad:", visibilidad);
-        console.log("Descripción:", descripcion);
-    }
+	function crearComunidad() {
+		console.log('Nombre:', nombre);
+		console.log('Visibilidad:', visibilidad);
+		console.log('Descripción:', descripcion);
+	}
 </script>
 
-<div>
-    <h1>Crear Comunidad</h1>
-    <label for="nombre">Nombre:</label>
-    <input type="text" id="nombre" bind:value={nombre} />
+<div class="crear-comunidad">
+	<form on:submit|preventDefault={crearComunidad}>
+		<LayoutGrid>
+			<Cell span={12}>
+				<h1>Crear Comunidad</h1>
+			</Cell>
 
-    <div>
-        <label>Visibilidad:</label>
-        <input type="radio" id="publico" name="visibilidad" value="publico" bind:group={visibilidad} />
-        <label for="publico">Público</label>
-        <input type="radio" id="privado" name="visibilidad" value="privado" bind:group={visibilidad} />
-        <label for="privado">Privado</label>
-    </div>
+			<Cell span={12}>
+				<Textfield
+					class="input-nombre"
+					required
+					variant="filled"
+					bind:value={nombre}
+					label="Nombre"
+				/>
+			</Cell>
 
-    <label for="descripcion">Descripción:</label>
-    <textarea id="descripcion" rows="4" bind:value={descripcion}></textarea>
+			<Cell span={12}>
+				<FormField>
+					<Radio bind:group={visibilidad} value="Publico" />
+					<Label slot="label">Pública</Label>
+				</FormField>
+				<FormField>
+					<Radio bind:group={visibilidad} value="Privado" />
+					<Label slot="label">Privada</Label>
+				</FormField>
+			</Cell>
 
-    <button on:click={crearComunidad}>Crear Comunidad</button>
+			<Cell span={12}>
+				<Textfield
+					textarea
+					bind:value={descripcion}
+					required
+					label="Descripción"
+					style="width: 100%;"
+				/>
+			</Cell>
+
+			<Cell span={12}>
+				<div class="button-container">
+					<Button variant="raised" type="submit">Crear Comunidad</Button>
+				</div>
+			</Cell>
+		</LayoutGrid>
+	</form>
 </div>
+
+<style lang="sass">
+    .crear-comunidad :global(.input-nombre)
+        width: 300px
+    
+    .button-container
+        width: 100%
+        display: flex
+        justify-content: flex-end
+</style>
